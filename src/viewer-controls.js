aa9836df851389a4611e2f37a1c25a930449c5e5
Gargi Jadaun <128454603+Gargijadaun@users.noreply.github.com>
2024-02-07 16:34:43 +0530

    gbb

--- src/viewer-controls.js
+++ src/viewer-controls.js
@@ -74,9 +74,9 @@ screenshotImage.id = "photo";
 
 // Set the position and percentage-based size of the screenshot image in the overlay
 screenshotImage.style.position = "fixed";
-screenshotImage.style.top = "10%";
-screenshotImage.style.left = "17%";
-screenshotImage.style.width = "75%";  // Use a percentage for width
+screenshotImage.style.top = "15%";
+screenshotImage.style.left = "12%";
+screenshotImage.style.width = "86%";  // Use a percentage for width
 screenshotImage.style.height = "auto";  // Let the height adjust proportionally
 
 // Append the screenshot image to the overlay
